b94831e43e7e214de9ebefce528f7807d218126b
SerujanSatkunanathan <144580005+SerujanSatkunanathan@users.noreply.github.com>
2025-07-18 12:43:34 +0530

    initial theme setups pubspec package setup

---
+++ colour_changer/lib/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.grey,
+    fontFamily: 'Duhit',
+  );
+}
