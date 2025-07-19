bf8de9c6e0544260dfaf8d9e74afe3d0568f7637
SerujanSatkunanathan <144580005+SerujanSatkunanathan@users.noreply.github.com>
2025-07-19 11:39:21 +0530

    added new svg and background images

--- colour_changer/lib/screens/main_screen.dart
+++ colour_changer/lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,13 +11,13 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  Color backgroundColor = Colors.black;
+  Color backgroundColour = Colors.black;
 
   //Colour changing function
   void colourChange() {
     final random = Random();
     setState(() {
-      backgroundColor = Color.fromARGB(
+      backgroundColour = Color.fromARGB(
         255,
         random.nextInt(255),
         random.nextInt(255),
@@ -26,7 +27,42 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    showIntroDialog();
+  }
+
+  // Intro dialog
+  void showIntroDialog() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Space Alert!"),
+            content: Text("Tap anywhere to change the space colour."),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("OK"),
+              ),
+            ],
+          );
+        },
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    bool isDark =
+        backgroundColour.computeLuminance() <
+        0.5; //to find the colour is dark or bright
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       top: true,
       child: GestureDetector(
@@ -34,8 +70,49 @@ class _MainScreenState extends State<MainScreen> {
         child: Container(
           height: double.infinity,
           width: double.infinity,
-          decoration: BoxDecoration(color: backgroundColor),
-          child: Column(children: [Text("Hii There")]),
+          decoration: BoxDecoration(color: backgroundColour),
+          child: Stack(
+            children: [
+              Positioned(
+                top: -screenHeight * 0.4,
+                left: 0,
+                right: 0,
+                child: SvgPicture.asset(
+                  "assets/images/moon.svg",
+                  width: screenWidth * 1,
+                  height: screenHeight * 0.8,
+                ),
+              ),
+              Positioned(
+                bottom: 10,
+                left: 10,
+                right: 10,
+                child: Transform.scale(
+                  scale: 1.2,
+                  child: Image.asset(
+                    "assets/images/16949.png",
+                    width: screenWidth * 0.5,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: screenHeight * 0.33,
+                left: 0,
+                right: 0,
+
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Hii There!",
+                    style: TextStyle(
+                      color: isDark ? Colors.white : Colors.black,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
